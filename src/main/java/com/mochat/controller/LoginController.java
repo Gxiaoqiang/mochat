70942149b2a7f6c0c4047c7be0890aba7baf7f3d
GWQ <GWQ@windows10.microdone.cn>
2018-07-17 22:46:45 +0800

    login修改

--- src/main/java/com/mochat/controller/LoginController.java
+++ src/main/java/com/mochat/controller/LoginController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,19 +18,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.mochat.cache.ThreadLocalCache;
 import com.mochat.model.ResponseEntity;
 import com.mochat.model.UserInfo;
+import com.mochat.redis.CustomRedisClusters;
 import com.mochat.service.impl.LoginService;
 import com.mochat.service.impl.VerificationCodeService;
-import com.rabbitmq.client.impl.Method;
-@Controller
-@RequestMapping(value = {"/login"})
-public class LoginController extends CommonHandler{
 
-	
-	@Autowired
-	private LoginService loginService;
-	
+import redis.clients.jedis.JedisCluster;
+
+@Controller
+@RequestMapping(value = { "/login" })
+public class LoginController extends CommonHandler {
+
 	@Autowired
 	private VerificationCodeService verificationCodeService;
+	@Autowired
+	private LoginService loginService;
+
+	@Autowired
+	private CustomRedisClusters customRedisCluster;
+
 	/**
 	 * 
 	 * @param request
@@ -41,55 +45,68 @@ public class LoginController extends CommonHandler{
 	 */
 	@ResponseBody
 	@RequestMapping("/register")
-	public ResponseEntity<Boolean> register(@RequestBody Map<String, String> paramMap)throws Exception{
-		
+	public ResponseEntity<Boolean> register(@RequestBody Map<String, String> paramMap) throws Exception {
+
 		return null;
 	}
-	
-	
+
 	@ResponseBody
 	@RequestMapping("/loginOut")
-	public ResponseEntity<Boolean> loginOut(HttpServletRequest request ,HttpServletResponse response )throws Exception{
-		
-		Boolean flag =  loginService.loginOut(request, response);
-		
+	public ResponseEntity<Boolean> loginOut(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		Boolean flag = loginService.loginOut(request, response);
+
 		return successHandle(true, flag, null);
 	}
-	
+
 	/**
 	 * ��¼
+	 * 
 	 * @param request
 	 * @param response
 	 * @return
 	 */
 	@ResponseBody
-	@RequestMapping(value= {"/loginIn"})
-	public ResponseEntity<UserInfo> login(HttpServletRequest request,HttpServletResponse response)throws Exception {
-		
+	@RequestMapping(value = { "/loginIn" })
+	public ResponseEntity<UserInfo> login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
 		String userMail = request.getParameter("email");
 		String passWord = request.getParameter("passWord");
+		String vdcode = request.getParameter("vdcode");
+		if (vdcode == null) {
+			return successHandle(false, null, "验证码错误");
+		}
+		String vdcodeId = request.getParameter("vdcodeId");
+		JedisCluster jedisCluster = customRedisCluster.getJedisCluster();
+		String vdcodeOld = jedisCluster.get(vdcodeId);
+		if (vdcodeOld == null) {
+			return successHandle(false, null, "验证码错误");
+		}
+		if (!StringUtils.equals(vdcode.trim(), vdcodeOld.trim())) {
+			return successHandle(false, null, "验证码错误");
+		}
 		UserInfo userInfo = new UserInfo();
-		
-		if(StringUtils.isEmpty(userMail)||StringUtils.isEmpty(passWord)) {
+
+		if (StringUtils.isEmpty(userMail) || StringUtils.isEmpty(passWord)) {
 			return successHandle(false, null, "邮箱或者密码不能为空！");
 		}
 		userInfo.setEmail(userMail);
 		userInfo.setUserPassWord(passWord);
 		userInfo = loginService.getUserInfo(userInfo, request, response);
-		
+
 		return successHandle(true, userInfo, null);
 	}
-	
+
 	@ResponseBody
-	@RequestMapping(value= {"/getUserInfo"})
-	public ResponseEntity<UserInfo> getUserInfo(){
+	@RequestMapping(value = { "/getUserInfo" })
+	public ResponseEntity<UserInfo> getUserInfo() {
 		UserInfo userInfo = ThreadLocalCache.get();
 		return successHandle(true, userInfo, null);
 	}
-	
-	@RequestMapping(value= {"/getVerificationImage"},method=RequestMethod.GET)
+
+	@RequestMapping(value = { "/getVerificationImage" }, method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<Map<String, Object>> getVerificationImage() throws ServletException, IOException{
+	public ResponseEntity<Map<String, Object>> getVerificationImage() throws ServletException, IOException {
 		Map<String, Object> map = verificationCodeService.encodeBase64ImgCode();
 		return successHandle(true, map, null);
 	}
--- src/main/webapp/login/js/login.js
+++ src/main/webapp/login/js/login.js
@@ -1,88 +1,200 @@
-﻿(function($){
-	$.fn.login = {
-			init:function(){
-				$.fn.login.verificationImage();
-				$.fn.login.bindEvent();
-			},
-			verificationImage:function(){
-				var url = "/login/getVerificationImage";
-				var config = {
-					"URL":url,
-					"METHOD":"get"
-				};
-				HTTPUtil.remoteInvoke(config,function(result){
-					var $verification = $(".login .verification");
-					$verification.attr("src",result.image);
-					$verification.attr("srand",result.sRand);
-				});
-			},
-			bindEvent:function(){
-				 $(".content .con_right .left").click(function (e) {
-				        $(this).css({ "color": "#333333", "border-bottom": "2px solid #2e558e" });
-				        $(".content .con_right .right").css({ "color": "#999999", "border-bottom": "2px solid #dedede" });
-				        $(".content .con_right ul .con_r_left").css("display", "block");
-				        $(".content .con_right ul .con_r_right").css("display", "none");
-				        if (fluCodeInterval == null || fluCheckCodeInterval == null) {
-				            show();
-				            flushQRCode();
-				            checkQRCodeStatus();
-				        }
-				    });
-				    $(".content .con_right .right").click(function (e) {
-				        $(this).css({ "color": "#333333", "border-bottom": "2px solid #2e558e" });
-				        $(".content .con_right .left").css({ "color": "#999999", "border-bottom": "2px solid #dedede" });
-				        $(".content .con_right ul .con_r_right").css("display", "block");
-				        $(".content .con_right ul .con_r_left").css("display", "none");
-				    });
-				    $('#btn_Login').click(function () {
-				    	var email = $('#userid').val();
-				    	var passWord = $('#pwd').val();
-				        if ($.trim(email) == '') {
-				            alert('请输入您的用户名');
-				            return false;
-				        } else if ($.trim(passWord) == '') {
-				            alert('请输入密码');
-				            return false;
-				        } else {
-				            var data = {
-				            	"email":email,
-				            	"passWord":passWord
-				            };
-				            $.ajax({
-				            	data:data,
-				            	url:"login/loginIn",
-				            	type : "post",
-								dataType : "json",	
-								success : function(result){
-									if(result.success == true){
-										var userInfo = result.data;
-										if(userInfo != null){
-											var randomId = mochat.utils.randomStr(33);
-											$("#user_form").attr("action","menu.html");
-											$("#random").val(randomId);
-											document.getElementById("user_form").submit();
-											var userInfoStr = JSON.stringify(userInfo);
-											sessionStorage.setItem("userInfo_"+randomId,userInfoStr);
-										}else{
-											alert(result.msg);
-										}
-									}else{
-										alert(result.msg);
-									}
-								},
-								fail : function(data){
-									errorHandler ? errorHandler(data) : "";
-								}
-				            });
-				        }
-				    });
+﻿(function($) {
 
-				    $("#flushLoginValiCode1,#flushLoginValiCode2").click(function () {
-				        $("#loginImgCode").attr("src", "/handler/GetLoginCode.ashx?" + Math.random());
-				    });
-				    $(".login .verification").click(function(){
-				    	$.fn.login.verificationImage();
-				    });
-			}
+	$.fn.login = {
+
+		init : function() {
+
+			$.fn.login.verificationImage();
+
+			$.fn.login.bindEvent();
+
+		},
+
+		verificationImage : function() {
+
+			var url = "/login/getVerificationImage";
+
+			var config = {
+
+				"URL" : url,
+
+				"METHOD" : "get"
+
+			};
+
+			HTTPUtil.remoteInvoke(config, function(result) {
+
+				var $verification = $(".login .verification");
+
+				$verification.attr("src", result.image);
+
+				$verification.attr("srand", result.sRand);
+
+			});
+
+		},
+
+		bindEvent : function() {
+
+			$(".content .con_right .left").click(
+					function(e) {
+
+						$(this).css({
+							"color" : "#333333",
+							"border-bottom" : "2px solid #2e558e"
+						});
+
+						$(".content .con_right .right").css({
+							"color" : "#999999",
+							"border-bottom" : "2px solid #dedede"
+						});
+
+						$(".content .con_right ul .con_r_left").css("display",
+								"block");
+
+						$(".content .con_right ul .con_r_right").css("display",
+								"none");
+
+						if (fluCodeInterval == null
+								|| fluCheckCodeInterval == null) {
+
+							show();
+
+							flushQRCode();
+
+							checkQRCodeStatus();
+
+						}
+
+					});
+
+			$(".content .con_right .right").click(
+					function(e) {
+
+						$(this).css({
+							"color" : "#333333",
+							"border-bottom" : "2px solid #2e558e"
+						});
+
+						$(".content .con_right .left").css({
+							"color" : "#999999",
+							"border-bottom" : "2px solid #dedede"
+						});
+
+						$(".content .con_right ul .con_r_right").css("display",
+								"block");
+
+						$(".content .con_right ul .con_r_left").css("display",
+								"none");
+
+					});
+
+			$('#btn_Login')
+					.click(
+							function() {
+
+								var email = $('#userid').val();
+								var passWord = $('#pwd').val();
+								var vdcode = $("#vdcode").val();
+								if ($.trim(email) == '') {
+									alert('请输入您的用户名');
+									return false;
+								}
+								if ($.trim(passWord) == '') {
+									alert('请输入密码');
+									return false;
+								}
+								if ($.trim(vdcode) == '') {
+									alert("请输入验证码！");
+									return false;
+								}
+								{
+									var data = {
+										"email" : email,
+										"passWord" : passWord,
+										"vdcode" : vdcode,
+										"vdcodeId" : $(".verification").attr(
+												"srand")
+
+									};
+
+									$
+											.ajax({
+												data : data,
+												url : "login/loginIn",
+												type : "post",
+												dataType : "json",
+												success : function(result) {
+													if (result.success == true) {
+														var userInfo = result.data;
+														if (userInfo != null) {
+															var randomId = mochat.utils
+																	.randomStr(33);
+															$("#user_form")
+																	.attr(
+																			"action",
+																			"menu.html");
+
+															$("#random").val(
+																	randomId);
+
+															document
+																	.getElementById(
+																			"user_form")
+																	.submit();
+
+															var userInfoStr = JSON
+																	.stringify(userInfo);
+
+															sessionStorage
+																	.setItem(
+																			"userInfo_"
+																					+ randomId,
+																			userInfoStr);
+
+														} else {
+
+															alert(result.msg);
+
+														}
+
+													} else {
+
+														alert(result.msg);
+
+													}
+
+												},
+
+												fail : function(data) {
+
+													errorHandler ? errorHandler(data)
+															: "";
+
+												}
+
+											});
+
+								}
+
+							});
+
+			$("#flushLoginValiCode1,#flushLoginValiCode2").click(
+					function() {
+
+						$("#loginImgCode").attr("src",
+								"/handler/GetLoginCode.ashx?" + Math.random());
+
+					});
+
+			$(".login .verification").click(function() {
+
+				$.fn.login.verificationImage();
+
+			});
+
+		}
+
 	}
-})(jQuery,window,undefined)
+
+})(jQuery, window, undefined)
